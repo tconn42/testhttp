66e907458902bd2e6155e7e0d0273d7062a6326e
Tim Connors <timconnors42@gmail.com>
2025-07-31 15:32:05 -0400

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const http = require('http');
+//const https = require('https');
+const express = require('express');
+
+const http_port = 80;
+//const https_port = 443;
+
+const app = express();
+
+// Listen for http traffic
+const server = app.listen(http_port, () =>
+{
+  console.log('HTTP Server listening on port: ' + http_port);
+});
+
+app.use(express.static('public'));
